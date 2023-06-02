d1d554a9522c630c0ebf8de39d274a34479bba0b
Sam Demeulemeester <samdemeulemeester@hotmail.com>
2023-06-02 21:41:41 +0200

    Initial commit

---
+++ src/main/java/org/samdml/utility/ubl/UblUtilityApplication.java
@@ -0,0 +1,13 @@
+package org.samdml.utility.ubl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UblUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UblUtilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/samdml/utility/ubl/UblUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samdml.utility.ubl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UblUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
